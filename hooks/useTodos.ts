2c9b4a51472d4e0d4069afd470b6f9a6d3e79158
Hryhorii Kachurovskyi <kachurovskyy123@gmail.com>
2025-04-15 18:56:31 +0300

    fixes

--- hooks/useTodos.ts
+++ hooks/useTodos.ts
@@ -1,106 +1,62 @@
 'use client'
 
 import { Todo } from '@/types/todo'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useCallback } from 'react'
 
 const TODOS_KEY = ['todos']
-const LOCAL_STORAGE_KEY = 'todos'
 
-export const useTodos = () => {
-  const [localTodos, setLocalTodos] = useState<Todo[]>([])
-  const [isLoading, setIsLoading] = useState(true)
+export function useTodos() {
+  const queryClient = useQueryClient()
 
-  useEffect(() => {
-    const stored = localStorage.getItem(LOCAL_STORAGE_KEY)
-    if (stored) {
-      setLocalTodos(JSON.parse(stored))
-      setIsLoading(false)
-    }
-  }, [])
-
-  useQuery<Todo[], Error>({
+  const { data = [], isLoading } = useQuery<Todo[]>({
     queryKey: TODOS_KEY,
     queryFn: async () => {
-      const res = await axios.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      const res = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
       return res.data
     },
-    enabled: localTodos.length === 0,
-    // @ts-ignore
-    onSuccess: (data: any) => {
-      const withIds = data.map((todo: any) => ({
-        ...todo,
-        id: Date.now() + Math.random(),
-      }))
-      setLocalTodos(withIds)
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(withIds))
-      setIsLoading(false)
-    },
-    onError: () => {
-      setIsLoading(false)
-    }
   })
 
-  const syncLocal = (todos: Todo[]) => {
-    setLocalTodos(todos)
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }
-
   const addMutation = useMutation({
-    mutationFn: (title: string) =>
-      axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', {
+    mutationFn: async (title: string) => {
+      const res = await axios.post<Todo>('https://jsonplaceholder.typicode.com/todos', {
         title,
         completed: false,
-      }),
-    onMutate: async (title) => {
-      const tempId = Math.floor(Math.random() * 1000000)
-      const newTodo = { id: tempId, title, completed: false }
-      const optimistic = [newTodo, ...localTodos]
-      syncLocal(optimistic)
-      return { optimistic, tempId }
+      })
+      return res.data
     },
-    onSuccess: (res, title, context) => {
-      const confirmed = localTodos.map(todo =>
-        todo.id === context?.tempId ? { ...res.data, id: context.tempId } : todo
-      )
-      syncLocal(confirmed)
-    },
-    onError: (_err, _title, context) => {
-      syncLocal(context?.optimistic || localTodos)
+    onSuccess: (todo) => {
+      const uniqueTodo = {
+        ...todo,
+        id: Date.now() + Math.floor(Math.random() * 1000),
+      }
+      queryClient.setQueryData<Todo[]>(TODOS_KEY, (prev = []) => [
+        uniqueTodo,
+        ...prev,
+      ])
     }
   })
 
   const deleteMutation = useMutation({
     mutationFn: (id: number) =>
       axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`),
-    onMutate: async (id) => {
-      const optimistic = localTodos.filter(todo => todo.id !== id)
-      syncLocal(optimistic)
-      return { optimistic }
+    onSuccess: (_, id) => {
+      queryClient.setQueryData<Todo[]>(TODOS_KEY, (prev = []) =>
+        prev.filter((todo) => todo.id !== id)
+      )
     },
-    onError: (_err, _id, context) => {
-      syncLocal(context?.optimistic || localTodos)
-    }
   })
 
-  const updateTodo = (id: number, newTitle: string) => {
-    const updated = localTodos.map(todo =>
-      todo.id === id ? { ...todo, title: newTitle } : todo
-    )
-    syncLocal(updated)
-  }
-
-  const reorderTodos = (newOrder: Todo[]) => {
-    syncLocal(newOrder)
-  }
+  const reorderTodos = useCallback((newOrder: Todo[]) => {
+    queryClient.setQueryData<Todo[]>(TODOS_KEY, newOrder)
+  }, [queryClient])
 
   return {
-    todos: localTodos,
+    todos: data,
     isLoading,
     addTodo: addMutation.mutate,
     deleteTodo: deleteMutation.mutate,
-    updateTodo,
-    reorderTodos
+    reorderTodos,
   }
 }
